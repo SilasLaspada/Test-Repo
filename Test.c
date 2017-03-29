9aca16fec2be22917c28214017985b2ee8cea75a
SilasLaspada <silasqwerty@ymail.com>
2017-03-29 08:48:22 -0600

    More changes to Test.c
    Test.c now prints "My name is Silas."

--- Test.c
+++ Test.c
@@ -5,7 +5,9 @@ int main()
 	int FavNumber = 42;
 	int TwoNumber = TimeTwo(FavNumber);
 	if (TwoNumber == 84) {
-		printf("Hello World!");
+		printf("Hello World!\n");
 	}
+	char name[] = "Silas";
+	printf("My name is %s.\n", name);
 	return 0;
 }
